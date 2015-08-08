2f60265386065fafb1277106aa2e6e68e701683e
nicholasruggeri <ruggerinicholas@gmail.com>
2015-08-08 18:33:39 +0200

    Update 2015-08-08T16:33:33.870Z

--- js/script.js
+++ js/script.js
@@ -195,15 +195,15 @@
 
         function init() {
             var $el = $('.section-header');
-            $('.site').removeClass('spinner');
             setTimeout(function () {
+                $('.site').removeClass('spinner');
                 TweenLite.to($el, 1, {
                     x:0,
                     ease: Expo.easeInOut,
                 });
                 TweenLite.to($('.header span'), 0.5, {
                     x:0,
-                    delay: 0.3,
+                    delay: 0.4,
                     ease: Expo.easeIn,
                     onComplete: function(){
                         $('h1,h2').css('color','rgba(255,255,255,1)')
