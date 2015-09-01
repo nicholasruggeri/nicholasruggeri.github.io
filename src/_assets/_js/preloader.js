f9c2c9cdb0cc36bade30162892a3ed2a95c198a2
nicholasruggeri <ruggerinicholas@gmail.com>
2015-09-01 23:57:49 +0200

    opt img

--- src/_assets/_js/preloader.js
+++ src/_assets/_js/preloader.js
@@ -1,29 +1,49 @@
 (function(exports) {
 
-    var preloader = (function() {
+    var Preloader = (function() {
 
-        function init() {
-            var $el = $('.section-header');
-            $('.site').removeClass('spinner');
+        var $site = $('.site'),
+            $header = $('.section-header'),
+            $animatedText = $('h1,h2'),
+            $animatedMask = $('.header span'),
+            $image = $('.content-image img');
+
+        function _initPreloader() {
+            $site.removeClass('spinner');
             setTimeout(function () {
-                TweenLite.to($el, 1, {
-                    x:0,
+                TweenLite.to($header, 1, {
+                    x: 0,
                     ease: Expo.easeInOut,
                 });
                 TweenLite.to($('.header span'), 0.5, {
-                    x:0,
+                    x: 0,
                     delay: 0.3,
                     ease: Expo.easeIn,
                     onComplete: function(){
-                        $('h1,h2').css('color','rgba(255,255,255,1)')
-                        TweenLite.to($('.header span'), 1, {
-                            x:'105%',
+                        $animatedText.css('color','rgba(255,255,255,1)')
+                        TweenLite.to($animatedMask, 1, {
+                            x: '105%',
                             ease: Expo.easeOut
                         });
                     }
                 });
             }, 0);
         }
+
+        function _loadImages() {
+            var _this, url;
+            $image.each(function(){
+                _this = $(this);
+                url = _this.attr( 'src');
+                _this.attr( 'srcset', url)
+            });
+        }
+
+        function init() {
+            _initPreloader();
+            _loadImages();
+        }
+
 
         return {
             init: init
@@ -34,6 +54,6 @@
         $(this).scrollTop(0);
     });
 
-    $(window).on('load', preloader.init);
+    $(window).on('load', Preloader.init);
 
 }(window));
